77b8fb10282970c86cc4c2e20fd7aed73badb938
Zoran Kocevski <zorankocevski@Zorans-MacBook-Air.local>
2015-12-26 21:17:18 +0100

    Add all rest files. The first completed version of the application.

---
+++ pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by Zoran Kocevski on 12/26/15.
+//  Copyright © 2015 Zoran Kocevski. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,29 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Zoran Kocevski on 12/20/15.
+//  Copyright © 2015 Zoran Kocevski. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
